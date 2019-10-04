5b577ae05c3bbc2fd21291c6766a96ef9338cf5f
sherhannn79 <54765500+sherhannn79@users.noreply.github.com>
2019-10-04 12:56:02 +0300

    Add files via upload

---
+++ bootrom/version.c
@@ -0,0 +1,10 @@
+#include "proxmark3.h"
+/* Generated file, do not edit */
+const struct version_information __attribute__((section(".version_information"))) version_information = {
+	VERSION_INFORMATION_MAGIC,
+	1,
+	1,
+	2,
+	"/",
+	"2019-10-03 06:58:14",
+};
